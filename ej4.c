461ac1a5f7b388a1be7eaa07f285ee26b2af3e8a
Alexander <88721696+Alexottokrause@users.noreply.github.com>
2021-11-23 11:58:25 -0300

    Create ej4.c

--- ej4.c
+++ ej4.c
@@ -1,27 +1,32 @@
-#include <stdio.h>
+#include<stdio.h>
 #include<stdlib.h>
 void recaudacion(int co[4][13],int rt,int rl);
-int main(){
-  int i,j;
-  int rt=0;
-  int rl=0;
-  int co[4][13];
-  for(i=1;i<=3;i++){
-    for (j=1;j<=12;j++)
-    {
-      printf("Indique recaudacion Linea %d, colectivo %d:",i,j);
-      scanf("%d",&co[i][j]);
-    }
-  }
-  recaudacion(co,rt,rl);
+void main(){
+    int co[4][13]={0};
+    int i,j;
+    int rt=0,rl=0;
+    do{
+        printf("Ingrese el numero de coche:");
+        scanf("%d",&j);
+        if(j>=1){
+        printf("Ingrese la linea:");
+        scanf("%d",&i);
+        printf("Ingrese la recaudacion:");
+        scanf("%d",&co[i][j]);
+        }
+        else{
+            break;
+        }
+    }while(j>=1);
+    recaudacion(co,rt,rl);
+ }
 
-}
-void recaudacion(int co[4][13],int rt,int rl){
+ void recaudacion(int co[4][13],int rt,int rl){
 int i,j;
 for(i=1;i<=3;i++){
     rl = 0;
     for (j=1;j<=12;j++){
-      printf("Recaudacion de la linea %d y colectivo %d es: %d \n",j, i, co[i][j]);
+      printf("Recaudacion de la linea %d y colectivo %d es: %d \n",i, j, co[i][j]);
       rt=rt+co[i][j];
       rl=rl+co[i][j];
     }
